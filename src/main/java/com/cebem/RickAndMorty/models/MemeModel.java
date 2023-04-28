593228e00ca9f8b844552eeae2249ed69bf912a1
jgl1201 <jorgegarciacebem@gmail.com>
2023-04-28 12:18:48 +0200

    first commit

---
+++ src/main/java/com/cebem/RickAndMorty/models/MemeModel.java
@@ -0,0 +1,30 @@
+package com.cebem.RickAndMorty.models;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "Meme")
+public class MemeModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    long id;
+
+    String category;
+    String url;
+    String description;
+    String author;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    Date createdAt;
+}
